b7553feeaec9f1291c88c436ea581b972e500de4
AkshatDahikar <akshatdahikar@gmail.com>
2024-12-02 11:50:04 +0530

    Added : Create Contact

--- src/AddressBookSystem/Address.java
+++ src/AddressBookSystem/Address.java
@@ -1,7 +1,38 @@
 package AddressBookSystem;
 
+import java.util.Scanner;
+
 public class Address {
 public static void main(String[] args) {
 	System.out.println("Welcome to Address Book Program in AddressBookMain class on Master Branch");
+	Scanner scanner = new Scanner(System.in);
+
+	System.out.print("Enter First Name: ");
+	String firstName = scanner.nextLine();
+
+	System.out.print("Enter Last Name: ");
+	String lastName = scanner.nextLine();
+
+	System.out.print("Enter Address: ");
+	String address = scanner.nextLine();
+
+	System.out.print("Enter City: ");
+	String city = scanner.nextLine();
+
+	System.out.print("Enter State: ");
+	String state = scanner.nextLine();
+	System.out.print("Enter ZIP: ");
+	String zip = scanner.nextLine();
+
+	System.out.print("Enter Phone Number: ");
+	String phoneNumber = scanner.nextLine();
+
+	System.out.print("Enter Email: ");
+	String email = scanner.nextLine();
+
+	ContactAddress contactAddress = new ContactAddress(firstName, lastName, address, city, state, zip, phoneNumber,
+			email);
+	contactAddress.showInfo();
+	scanner.close();
 }
 }
